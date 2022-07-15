755c1c5d7f64b4303fe793e4bff72088e4c5445a
Lucas Massayuki Cruz <lucasofficialcruz95@gmail.com>
2022-07-15 13:54:07 -0300

    Criando um método para obtermos os benifícios de reaproveitamento e delegação

--- Introducao_Orientada_a_Objetos/src/entities/Triangle.java
+++ Introducao_Orientada_a_Objetos/src/entities/Triangle.java
@@ -1,7 +1,12 @@
-package entities;
+package entities;//pacote da classe
 
-public class Triangle {//Criando a classe e a difinição do tipo
-	public double a;
-	public double b;
+public class Triangle {//<- nome da classe
+	public double a;//<- "public" indica que o atributo ou método pode ser usado em outros arquivos
+	public double b;//<- double a,b,c; Atributos da classe
 	public double c;
+	
+	public double area() {//area() nome do método 
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+	}
 }
