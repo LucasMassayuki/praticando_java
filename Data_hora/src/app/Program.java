0ff94e8cdc7b0181cdede6c614dc62d0b532e7cf
LucasMassayuki <lucasofficialcruz95@gmail.com>
2022-08-13 22:49:50 -0300

    Convertendo data-hora para texto

--- Data_hora/src/app/Program.java
+++ Data_hora/src/app/Program.java
@@ -3,38 +3,35 @@ package app;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");//difinindo as ordens 
-		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");//Horas:minutos
-		
-		LocalDate d01 = LocalDate.now();// ano-mês-data
-		LocalDateTime d02 = LocalDateTime.now();// ano-mês-data-horas-minutos-segundos
-		Instant d03 = Instant.now();//data-hora global fuso de 3 horas de Londres
-		
 		LocalDate d04 = LocalDate.parse("2022-07-07");//com .parse consegue manipular a data
 		LocalDateTime d05 = LocalDateTime.parse("2022-07-07T12:00:00");// 
 		Instant d06 = Instant.parse("2022-07-07T12:00:00Z");//
 		
-		LocalDate d07 = LocalDate.parse("07/07/2022", fmt1);
-		LocalDate d08 = LocalDate.parse("07/10/2022 07:07", fmt2);
-		LocalDate d09 = LocalDate.of(2022, 7, 07);
-		LocalDateTime d10 = LocalDateTime.of(2022, 7, 8, 1, 1);
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+		//fmt3 pega o fuso do usuario do pc 
+		DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+		DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;//Com fuso de UTC
 		
-		System.out.println("d01 = " + d01.toString());
-		System.out.println("d02 = " + d02.toString());
-		System.out.println("d03 = " + d03.toString());
-		System.out.println("d04 = " + d04.toString());
-		System.out.println("d05 = " + d05.toString());
+		System.out.println("d04 = " + d04.format(fmt1));//3 formas pra instanciar 
+		System.out.println("d04 = " + fmt1.format(d04));
+		System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		System.out.println("d05 = " + d05.format(fmt1));
+		System.out.println("d05 = " + d05.format(fmt2));
+		System.out.println("d05 = " + d05.format(fmt4));
+		
+		System.out.println("d06 = " + fmt3.format(d06));
+		System.out.println("d06 = " + fmt5.format(d06));
 		System.out.println("d06 = " + d06.toString());
-		System.out.println("d07 = " + d07.toString());
-		System.out.println("d08 = " + d08.toString());
-		System.out.println("d09 = " + d09.toString());
-		System.out.println("d10 = " + d10.toString());
 	}
 
 }
