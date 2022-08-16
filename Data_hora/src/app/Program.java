39a9a8bd1eb9605c798e3da5fa50a3d34b8cea81
LucasMassayuki <lucasofficialcruz95@gmail.com>
2022-08-16 19:54:17 -0300

    Maipulando uma data com Calendar - Somando uma unidade de tempo

--- Data_hora/src/app/Program.java
+++ Data_hora/src/app/Program.java
@@ -3,42 +3,24 @@ package app;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
 
-		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
-		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		sdf3.setTimeZone(TimeZone.getTimeZone("GMT"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
-		Date x1 = new Date();//imprime a data de agora
-		Date x2 = new Date(System.currentTimeMillis());
-		Date x3 = new Date(0L);//0milisegundo
+		Date d = Date.from(Instant.parse("2022-08-16T19:42:07Z"));
 		
+		System.out.println(sdf.format(d));
 		
-		Date y1 = sdf1.parse("12/08/2022");
-		Date y2 = sdf2.parse("16/08/2022 19:05:35");
-		Date y3 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		d = cal.getTime();
 		
-		System.out.println("----------------------");
-		System.out.println("x1: " + sdf2.format(x1));
-		System.out.println("x2: " + sdf2.format(x2));
-		System.out.println("x3: " + sdf2.format(x3));
-		System.out.println("y1: " + sdf2.format(y1));
-		System.out.println("y2: " + sdf2.format(y2));
-		System.out.println("y3: " + sdf2.format(y3));
-		
-		System.out.println("----------------------");
-		System.out.println("x1: " + sdf3.format(x1));
-		System.out.println("x2: " + sdf3.format(x2));
-		System.out.println("x3: " + sdf3.format(x3));
-		System.out.println("y1: " + sdf3.format(y1));
-		System.out.println("y2: " + sdf3.format(y2));
-		System.out.println("y3: " + sdf3.format(y3));
-		
+		System.out.println(sdf.format(d));
 	}
 }
