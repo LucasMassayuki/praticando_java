5dfc4e0cbbfa2387d42c5aa2d20278e48d846bf3
LucasMassayuki <lucasofficialcruz95@gmail.com>
2022-08-16 19:58:47 -0300

    Obtendo uma unidade de tempo

--- Data_hora/src/app/Program.java
+++ Data_hora/src/app/Program.java
@@ -12,15 +12,17 @@ public class Program {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
-		Date d = Date.from(Instant.parse("2022-08-16T19:42:07Z"));
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
 		
 		System.out.println(sdf.format(d));
 		
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(d);
-		cal.add(Calendar.HOUR_OF_DAY, 4);
-		d = cal.getTime();
 		
-		System.out.println(sdf.format(d));
+		int minutes = cal.get(Calendar.MINUTE);
+		int month = 1 + cal.get(Calendar.MONTH);
+		
+		System.out.println("Minutes: " + minutes);
+		System.out.println("Month: " + month);
 	}
 }
