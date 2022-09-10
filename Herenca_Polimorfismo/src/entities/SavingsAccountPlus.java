f71d3e9fa5687c555d75c5f847019996443cea33
LucasMassayuki <lucasofficialcruz95@gmail.com>
2022-09-09 22:54:51 -0300

    Introdução ao polimorfismo

--- Herenca_Polimorfismo/src/entities/SavingsAccountPlus.java
+++
@@ -1,8 +0,0 @@
-package entities;
-
-public class SavingsAccountPlus extends SavingsAccount{
-	@Override
-	public void withdraw(double amount) {//não consegue utilizar por ter final na hernça 
-		balance -= amount + 2.0;
-	}
-}
