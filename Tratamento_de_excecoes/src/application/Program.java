62d412fd35a51c691efd800419c79b688c1c2f3b
LucasMassayuki <lucasofficialcruz95@gmail.com>
2022-09-21 17:08:09 -0300

    Pilha de chamadas de métodos (Stack trace)

--- Tratamento_de_excecoes/src/application/Program.java
+++ Tratamento_de_excecoes/src/application/Program.java
@@ -6,6 +6,19 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		method1();
+		
+		System.out.println("End of program");
+		
+	}
+	public static void method1() {
+		System.out.println("***METHO1 START***");
+		method2();
+		System.out.println("***METHO1 END***");
+	}
+	public static void method2() {
+		System.out.println("***METHO2 START***");
 		Scanner sc = new Scanner(System.in);
 		try {
 			String[] vect = sc.nextLine().split(" ");
@@ -14,12 +27,13 @@ public class Program {
 		}
 		catch(ArrayIndexOutOfBoundsException e) {//caso digite um numero fora da posição
 			System.out.println("invalid position!");
+			e.printStackTrace();//imprime na tela o rastriamento de Stack
+			sc.next();
 		}
 		catch(InputMismatchException e) {//caso digite letras na hora de digitar int position = sc.extDouble();
 			System.out.println("input error");
 		}
-		System.out.println("End of program");
 		sc.close();
+		System.out.println("***METHO2 END***");
 	}
-
 }
