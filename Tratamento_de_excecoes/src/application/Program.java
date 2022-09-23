f4d74fdb08f742d42705930458a7f1c683fd8f7f
LucasMassayuki <lucasofficialcruz95@gmail.com>
2022-09-22 21:58:49 -0300

    Criando exceções personalizadas

--- Tratamento_de_excecoes/src/application/Program.java
+++ Tratamento_de_excecoes/src/application/Program.java
@@ -1,29 +1,9 @@
 package application;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class Program {
 
 	public static void main(String[] args) {
-		File file = new File("C:\\temp\\in.text");//se tiver algo escrito vai aparecer
-		Scanner sc = null;
-		try {
-			sc = new Scanner(file);
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
-			}
-		}
-		catch(FileNotFoundException e) {
-			System.out.println("Erroropening file: " + e.getMessage());//caso não tem, vai aparecer este  
-		}
-		finally {
-			if(sc != null) {
-				sc.close();
-			}
-			System.out.println("Finally block executed");
-		}
+	
 		
 	}
 	
